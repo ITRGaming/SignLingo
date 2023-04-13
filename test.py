108d43d74cc97928cdf98acc0c033791e365866c
ITRGaming <raghavbhutra29@gmail.com>
2023-04-13 21:44:08 +0530

    SignLingo

--- test.py
+++ test.py
@@ -13,7 +13,8 @@ imgSize = 300
 
 folder = "Data/C"
 counter = 0
-labels = ["A", "B", "C"]
+labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+          "W", "X", "Y", "Z"]
 
 while True:
     success, img = cap.read()
@@ -50,7 +51,8 @@ while True:
             imgWhite[hGap:hCal + hGap, :] = imgResize
             prediction, index = classifier.getPrediction(imgWhite)
 
-        cv2.rectangle(imgOutput, (x - offset, y - offset - 50), (x - offset + 90, y - offset - 50 + 50), (255, 0, 255), cv2.FILLED)
+        cv2.rectangle(imgOutput, (x - offset, y - offset - 50), (x - offset + 90, y - offset - 50 + 50), (255, 0, 255),
+                      cv2.FILLED)
         cv2.putText(imgOutput, labels[index], (x, y - 27), cv2.FONT_HERSHEY_COMPLEX, 1.7, (255, 255, 255), 2)
         cv2.rectangle(imgOutput, (x-offset, y-offset), (x + w+offset, y + h+offset), (255, 0, 255), 4)
 
